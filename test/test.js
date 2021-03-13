2ddbc208fa34dfcda2d44e955bc6d84004a01bcc
DESKTOP-M3OCFUV\Wesley Alvarenga <letotech@outlook.com>
2021-03-13 00:06:02 -0300

    fix bug directories

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var request = require('supertest');
-
-var app = require('../index.js');
-
-describe('GET /', function(){
-    it('response with DevOps. Wesley Alvarenga.', function(done){
-        request(app).get('/').expect('DevOps. Wesley Alvarenga.', done);
-    });
-});
