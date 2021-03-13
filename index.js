b5a8ff5ed6a8a4a00019dde1b74705057a20463b
DESKTOP-M3OCFUV\Wesley Alvarenga <letotech@outlook.com>
2021-03-12 23:51:02 -0300

    Test Jenkins

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res){
-    res.send('Hey World');
+    res.send('DevOps. Wesley Alvarenga.');
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('supertest');
 var app = require('../index.js');
 
 describe('GET /', function(){
-    it('response with Hello World', function(done){
-        request(app).get('/').expect('Hey World', done);
+    it('response with DevOps. Wesley Alvarenga.', function(done){
+        request(app).get('/').expect('DevOps. Wesley Alvarenga.', done);
     });
 });
